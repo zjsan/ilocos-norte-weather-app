ed610dfa63fc7795ef57b4d249984f30b893f8a2
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-07-06 16:41:09 +0800

    location data

---
+++ ilocos-weather-app/src/data/locations.js
@@ -0,0 +1,9 @@
+export const locations = [
+  { name: "Laoag City", lat: 18.1956, lon: 120.592 },
+  { name: "Batac", lat: 18.0554, lon: 120.5646 },
+  { name: "Paoay", lat: 18.0625, lon: 120.5152 },
+  { name: "Pagudpud", lat: 18.5667, lon: 120.7833 },
+  { name: "Dingras", lat: 18.1000, lon: 120.7000 },
+  { name: "Burgos", lat: 18.5167, lon: 120.6333 },
+
+];
