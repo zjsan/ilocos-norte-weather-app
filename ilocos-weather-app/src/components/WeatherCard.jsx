41e19e2b3a75ba4068780747c9e1f1b6b6fb1c45
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-07-07 16:04:22 +0800

    Update WeatherCard.jsx

--- ilocos-weather-app/src/components/WeatherCard.jsx
+++ ilocos-weather-app/src/components/WeatherCard.jsx
@@ -5,9 +5,9 @@ const WeatherCard = ({ location, weather }) => {
   return (
     <div className="w-full max-w-md mx-auto flex flex-col items-center">
       <img src={rain} alt="sunny-sky image" className="weather-icons max-w-full h-auto" />
-      <h2 className="text-white font-semibold">{location.name}</h2>
-      <p className="text-white text-lg">🌡️ {weather.temperature}°C</p>
-      <p className="text-sm text-white">Wind: {weather.windspeed} km/h</p>
+      <h2 className="weather-location text-white text-4xl font-semibold">{location.name}</h2>
+      <p className="weather-temperature text-white text-xl">🌡️ {weather.temperature}°C</p>
+      <p className="text-base text-white">Wind: {weather.windspeed} km/h</p>
     </div>
 
   );
