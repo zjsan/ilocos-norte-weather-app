bc3c98952796c4c260b7611eb7923f053d85fd6f
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-07-10 15:01:21 +0800

    displaying relevant weather data

--- ilocos-weather-app/src/components/WeatherCard.jsx
+++ ilocos-weather-app/src/components/WeatherCard.jsx
@@ -3,12 +3,14 @@ import rain from '../assets/heavy-rain.png';
 
 const WeatherCard = ({ location, weather }) => {
   return (
-    <div className="w-full max-w-md mx-auto flex flex-col items-center">
-      <img src={rain} alt="sunny-sky image" className="weather-icons max-w-full h-auto" />
+    <div className="mt-6 space-y-4 text-center">
       <h2 className="weather-location text-white text-4xl font-semibold">{location.name}</h2>
-      <p className="weather-temperature text-white text-xl">🌡️ {weather.weather_code}°C</p>
-      <p className="weather-temperature text-white text-xl">🌡️ {weather.temperature}°C</p>
-      <p className="text-white text-lg">💨 {weather.windspeed} km/h</p>
+      <p className="weather-temperature text-white text-xl">Date: {weather.date}</p>
+      <p className="weather-temperature text-white text-xl">Time: {weather.time}</p>
+      <p className="weather-temperature text-white text-xl">{weather.weatherType}</p>
+      <p className="weather-temperature text-white text-xl">Temperature: {weather.temperature}</p>
+      <p className="weather-temperature text-white text-xl">Humidity: {weather.humidity}</p>
+      <p className="text-white text-lg">Wind Speed: {weather.windSpeed} km/h</p>
     
     </div>
 
