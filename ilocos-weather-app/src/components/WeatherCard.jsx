47f5dcaa65f34cb083fd2ac27380965d8d191d54
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-07-12 16:46:07 +0800

    Update WeatherCard.jsx

--- ilocos-weather-app/src/components/WeatherCard.jsx
+++ ilocos-weather-app/src/components/WeatherCard.jsx
@@ -3,19 +3,20 @@ import rain from "../assets/heavy-rain.png";
 
 const WeatherCard = ({ location, weather }) => {
   return (
-    <div className="mt-2 space-y-4 text-center p-5 bg-transparent ring-0">
-      <p className="weather-temperature text-white text-xl">
+    <div className="mt-2 space-y-3 text-center p-5 bg-transparent ring-0">
+      <p className="weather-temperature text-white text-xl drop-shadow-sm"> Today, {weather.date}</p>
+      <p className="weather-temperature text-white text-xl drop-shadow-sm">{weather.time}
+      </p>
+      <p className="weather-temperature text-white text-5xl drop-shadow-sm">{weather.temperature}°
+      </p>
+      <p className="weather-temperature text-white text-xl drop-shadow-sm">
         {weather.weatherType}
       </p>
-      <p className="weather-temperature text-white text-xl">{weather.temperature} °
-      </p>
-      <p className="weather-temperature text-white text-xl">{weather.date}</p>
-      <p className="weather-temperature text-white text-xl">{weather.time}
-      </p>
-      <p className="weather-temperature text-white text-xl">
+      
+      <p className="weather-temperature text-white text-xl drop-shadow-sm">
         Humidity: {weather.humidity}
       </p>
-      <p className="text-white text-lg">Wind Speed: {weather.windSpeed} km/h</p>
+      <p className="text-white text-lg drop-shadow-sm">Wind Speed: {weather.windSpeed} km/h</p>
     </div>
   );
 };
