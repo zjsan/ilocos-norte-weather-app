46bbb4a3af74db65c58c32836a1b4b4e8902cf2b
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-07-18 17:22:22 +0800

    added functionality for dynamic weather image

--- ilocos-weather-app/src/components/WeatherCard.jsx
+++ ilocos-weather-app/src/components/WeatherCard.jsx
@@ -1,35 +1,73 @@
 import sunny from "../assets/sun.png";
 import rain from "../assets/heavy-rain.png";
+import clouds from "../assets/clouds.png";
+import storm from "../assets/storm.png";
 
-
-
-const WeatherCard = ({ location, weather }) => {
-
-  let weatherType = weather.weatherType
-
-  function renderWeatherImage({weatherType}){
-
+const WeatherCard = ({ weather }) => {
+  function renderWeatherImage() {
+    if (
+      weather.weatherType === "Clear sky" ||
+      weather.weatherType === "Mainly clear"
+    ) {
+      return sunny;
+    } else if (
+      weather.weatherType === "Partly cloudy" ||
+      weather.weatherType === "Overcast" ||
+      weather.weatherType === "Fog" ||
+      weather.weatherType === "Depositing rime fog"
+    ) {
+      return clouds;
+    } else if (
+      weather.weatherType === "Drizzle: Light" ||
+      weather.weatherType === "Drizzle: Moderate" ||
+      weather.weatherType === "Drizzle: Dense intensity" ||
+      weather.weatherType === "Freezing Drizzle: Light" ||
+      weather.weatherType === "Freezing Drizzle: Dense intensity" ||
+      weather.weatherType === "Rain: Slight" ||
+      weather.weatherType === "Rain: Moderate" ||
+      weather.weatherType === "Rain: Heavy intensity" ||
+      weather.weatherType === "Freezing Rain: Light" ||
+      weather.weatherType === "Freezing Rain: Heavy intensity" ||
+      weather.weatherType === "Rain showers: Slight" ||
+      weather.weatherType === "Rain showers: Moderate" ||
+      weather.weatherType === "Rain showers: Violent"
+    ) {
+      return rain;
+    } else {
+      return storm;
+    }
   }
 
+  console.log(weather.weatherType);
   return (
     <div className="mt-1 space-y-3 text-center p-5 bg-transparent ring-0">
-      <p className="weather-temperature text-white text-xl drop-shadow-sm"> Today, {weather.date}</p>
-      <p className="weather-temperature text-white text-xl drop-shadow-sm">{weather.time}
+      <p className="weather-temperature text-white text-xl drop-shadow-sm">
+        {" "}
+        Today, {weather.date}
       </p>
-      <img src={rain} className="weather-icon" alt="Weather Icon" />
-      <p className="weather-temperature text-white text-5xl drop-shadow-sm">{weather.temperature}°
+      <p className="weather-temperature text-white text-xl drop-shadow-sm">
+        {weather.time}
+      </p>
+      <img
+        src={renderWeatherImage()}
+        className="weather-icon"
+        alt="Weather Icon"
+      />
+
+      <p className="weather-temperature text-white text-5xl drop-shadow-sm">
+        {weather.temperature}°
       </p>
       <p className="weather-temperature text-white text-xl drop-shadow-sm">
         {weather.weatherType}
       </p>
-      
       <div className="flex flex-row justify-around text-base w-full">
         <p className="weather-temperature text-white drop-shadow-sm">
-        Humidity: {weather.humidity}
-      </p>
-      <p className="text-white  drop-shadow-sm">Wind Speed: {weather.windSpeed} km/h</p>
+          Humidity: {weather.humidity}
+        </p>
+        <p className="text-white  drop-shadow-sm">
+          Wind Speed: {weather.windSpeed} km/h
+        </p>
       </div>
-      
     </div>
   );
 };
