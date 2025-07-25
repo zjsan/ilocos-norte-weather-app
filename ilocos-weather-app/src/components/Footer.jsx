56d2fae58b656ae4834aa3c6d9d2f708e62390d1
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-07-25 15:57:55 +0800

    Create Footer.jsx

---
+++ ilocos-weather-app/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="text-white text-center mt-5">
+      <p>© Amianan Forecast {new Date().getFullYear()}. All rights reserved.</p>
+    </div>
+  );
+};
