188a099f4594b48cef31ae905c74bd0ff96f26a1
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-04 20:48:38 +0800

    refactoring code for react-router

--- ilocos-weather-app/src/components/Home.jsx
+++ ilocos-weather-app/src/components/Home.jsx
@@ -1,13 +1,64 @@
-import LocationSelector from "/components/LocationSelector";
-import WeatherCard from "/components/WeatherCard";
-import { locations } from "./data/locations";
-import { weatherCodeMap } from "./data/weathercode";
-import { Footer } from "/components/Footer";
+import LocationSelector from "../components/LocationSelector";
+import WeatherCard from "../components/WeatherCard";
+import { locations } from "../data/locations";
+import { weatherCodeMap } from "../data/weathercode";
+import { Footer } from "../components/Footer";
 
-
-export const Home = () => {
+export const Home = ({
+  selectedLocation,
+  setSelectedLocation,
+  weatherData,
+  loading,
+  error,
+  backgroundClass,
+}) => {
   return (
-    <div>Home</div>
-  )
-}
+    <div
+      className={`bg-gradient-to-br ${backgroundClass} min-h-screen p-2 flex flex-col items-center`}
+    >
+      <div className="text-center mt-11">
+        <h1 className="text-3xl font-bold text-white drop-shadow-sm">
+          Amianan Forecast
+        </h1>
+        <h2 className=" text-white mt-2 mb-4 drop-shadow-sm">
+          Ilocos Norte Weather App
+        </h2>
+      </div>
 
+      <LocationSelector
+        selected={selectedLocation}
+        onChange={(value) => {
+          setSelectedLocation(value);
+        }}
+        locations={locations}
+      />
+
+      {loading && (
+        <div className="text-center text-white font-semibold text-lg mt-4">
+          Loading weather data...
+        </div>
+      )}
+
+      {error && (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-md relative mt-4"
+          role="alert"
+        >
+          <strong className="font-bold">Error!</strong>
+          <span className="block sm:inline ml-2">{error}</span>
+        </div>
+      )}
+
+      {weatherData && (
+        <div className="mt-1">
+          <WeatherCard
+            location={locations.find((l) => l.name === selectedLocation)}
+            weather={weatherData}
+          />
+        </div>
+      )}
+
+      <Footer />
+    </div>
+  );
+};
