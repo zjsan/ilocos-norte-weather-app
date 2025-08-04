50b74fc1810c3a03d7e98506ed99fe785341ee4f
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-04 20:26:44 +0800

    Create Home.jsx

---
+++ ilocos-weather-app/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import LocationSelector from "/components/LocationSelector";
+import WeatherCard from "/components/WeatherCard";
+import { locations } from "./data/locations";
+import { weatherCodeMap } from "./data/weathercode";
+import { Footer } from "/components/Footer";
+
+
+export const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
