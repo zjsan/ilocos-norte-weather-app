843b0d8e578c558630733624566aa89e9d3257d1
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-05 16:03:57 +0800

    Update Home.jsx

--- ilocos-weather-app/src/components/Home.jsx
+++ ilocos-weather-app/src/components/Home.jsx
@@ -61,15 +61,17 @@ export const Home = ({
       <Footer />
 
       <button className="btn 	
-bg-gray-300" onClick={()=>document.getElementById('my_modal_3').showModal()}>About the App</button>
+bg-gray-300 " onClick={()=>document.getElementById('my_modal_3').showModal()}>About the App</button>
 <dialog id="my_modal_3" className="modal">
   <div className="modal-box">
     <form method="dialog">
       {/* if there is a button in form, it will close the modal */}
       <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
     </form>
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+    <h3 className="font-bold text-lg text-center">Amianan Forecast</h3>
+    <p className="py-4">  This is a weather application for the different cities and municipalities in Ilocos Norte, Philippines. It fetches real-time weather data
+        from the Open-Meteo API. The application can automatically detect your current location
+        (with your permission) or you can manually select a city from the dropdown list.</p>
   </div>
 </dialog>
     </div>
