f3209b85c9570ead8a8dff2b260a9912840eeb15
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-11 16:01:28 +0800

    Update Home.jsx

--- ilocos-weather-app/src/components/Home.jsx
+++ ilocos-weather-app/src/components/Home.jsx
@@ -14,26 +14,36 @@ export const Home = ({
   backgroundClass,
 }) => {
   return (
-    
     <div
       className={`bg-gradient-to-br ${backgroundClass} min-h-screen p-2 flex flex-col items-center`}
     >
-
       <div className="flex justify-end w-full">
-          <button className="cursor-pointer" onClick={()=>document.getElementById('my_modal_3').showModal()}><img className="w-10 " src={alert} alt="alert icon"/></button>
-<dialog id="my_modal_3" className="modal">
-  <div className="modal-box">
-    <form method="dialog">
-      {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <h3 className="font-bold text-lg text-center">Amianan Forecast</h3>
-    <p className="py-4 text-justify">  This is a weather application for the different cities and municipalities in Ilocos Norte, Philippines. It fetches real-time weather data
-        from the Open-Meteo API which can be accessed using this link: https://open-meteo.com/. The application can automatically detect your current location
-        (with your permission) or you can manually select a city from the dropdown list.</p>
-  </div>
-</dialog>
-
+        <button
+          className="cursor-pointer"
+          onClick={() => document.getElementById("my_modal_3").showModal()}
+        >
+          <img className="w-10 " src={alert} alt="alert icon" />
+        </button>
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <h3 className="font-bold text-lg text-center">Amianan Forecast</h3>
+            <p className="py-4 text-justify">
+              {" "}
+              This is a weather application for the different cities and
+              municipalities in Ilocos Norte, Philippines. It fetches real-time
+              weather data from the Open-Meteo API which can be accessed using
+              this link: https://open-meteo.com/. The application can
+              automatically detect your current location (with your permission)
+              or you can manually select a city from the dropdown list.
+            </p>
+          </div>
+        </dialog>
       </div>
       <div className="text-center mt-11">
         <h1 className="text-3xl font-bold text-white drop-shadow-sm">
