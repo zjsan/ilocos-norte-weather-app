bfef3a36e45976c133f864fc11761b31c74ffd10
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-11 16:32:17 +0800

    Update Dashboard.jsx

--- ilocos-weather-app/src/components/Dashboard.jsx
+++ ilocos-weather-app/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 export const Dashboard = () => {
   return (
-    <div className="bg-gray-100 p-8 rounded-xl w-screen h-screen max-w-lg text-center">
+    <div className="bg-gray-100 p-8 rounded-xl w-screen h-screen text-center">
       <h1 className="text-3xl font-bold text-gray-900 mb-4">About This App</h1>
       <p className="text-lg text-gray-700">hello</p>
     </div>
