cf050c1f649216936b2ba8b8f52299fd01ec18dd
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-12 16:07:01 +0800

    Update Dashboard.jsx

--- ilocos-weather-app/src/components/Dashboard.jsx
+++ ilocos-weather-app/src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import weathericon from "../assets/weather.png";
 export const Dashboard = () => {
   return (
     <div className="bg-gradient-to-br from-blue-400 to-purple-600 p-2 w-screen h-screen text-center">
-      <div className="flex flex-row items-center">
+      <div className="flex flex-row items-center  w-screen">
         <h1 className="text-3xl font-bold text-gray-900 mb-4 text-white">
           Amianan Forecast Dashboard
         </h1>
