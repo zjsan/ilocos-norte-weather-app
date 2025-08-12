54b9e428bd0ee8f730f99f31743969cea3d68e7c
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-12 16:01:52 +0800

    Update Dashboard.jsx

--- ilocos-weather-app/src/components/Dashboard.jsx
+++ ilocos-weather-app/src/components/Dashboard.jsx
@@ -1,10 +1,13 @@
+import weathericon from "../assets/weather.png";
+
 export const Dashboard = () => {
   return (
     <div className="bg-gradient-to-br from-blue-400 to-purple-600 p-8 rounded-xl w-screen h-screen text-center">
       <h1 className="text-3xl font-bold text-gray-900 mb-4 text-white">
         Amianan Forecast Dashboard
       </h1>
-      <p className="text-lg text-white">hello</p>
+
+      <div className="flex flex-row items-center"></div>
     </div>
   );
 };
