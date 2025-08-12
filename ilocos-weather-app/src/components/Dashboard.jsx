84e702ca6cd9c03290a6101d62f32c1d8203c453
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-12 15:50:50 +0800

    Update Dashboard.jsx

--- ilocos-weather-app/src/components/Dashboard.jsx
+++ ilocos-weather-app/src/components/Dashboard.jsx
@@ -2,7 +2,7 @@ export const Dashboard = () => {
   return (
     <div className="bg-gradient-to-br from-blue-400 to-purple-600 p-8 rounded-xl w-screen h-screen text-center">
       <h1 className="text-3xl font-bold text-gray-900 mb-4 text-white">
-        About This App
+        Amianan Forecast Dashboard
       </h1>
       <p className="text-lg text-white">hello</p>
     </div>
