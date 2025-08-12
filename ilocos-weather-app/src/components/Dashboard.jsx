6199e6ad00b8f1e4e54526ed4bb6c848c90a5c75
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-12 16:13:07 +0800

    adjusting flexbox in the header div

--- ilocos-weather-app/src/components/Dashboard.jsx
+++ ilocos-weather-app/src/components/Dashboard.jsx
@@ -3,16 +3,12 @@ import weathericon from "../assets/weather.png";
 export const Dashboard = () => {
   return (
     <div className="bg-gradient-to-br from-blue-400 to-purple-600 p-2 w-screen h-screen text-center">
-      <div className="flex flex-row items-center  w-screen">
+      <div className="flex flex-row items-center  w-full justify-around items-end">
         <h1 className="text-3xl font-bold text-gray-900 mb-4 text-white">
           Amianan Forecast Dashboard
         </h1>
 
-        <img
-          src={weathericon}
-          className="weather-icon mb-3 mx-auto"
-          alt="Weather Icon"
-        />
+        <img src={weathericon} className="w-24" alt="Weather Icon" />
       </div>
     </div>
   );
