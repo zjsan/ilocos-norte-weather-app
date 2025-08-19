1a09c2ad6e62a0bcba597ef233de2d9365874c18
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-08-19 16:07:17 +0800

    trying to change dropdown color

--- ilocos-weather-app/src/components/LocationSelector.jsx
+++ ilocos-weather-app/src/components/LocationSelector.jsx
@@ -6,16 +6,15 @@ const LocationSelector = ({ selected, onChange }) => (
       className="border border-white bg-blue-100 text-black px-3 py-3 rounded-lg w-full max-w-sm"
       aria-label="Select Location"
       data-testid="location-selector"
-      value={selected ?? ''}
+      value={selected ?? ""}
       onChange={(e) => onChange(e.target.value)}
     >
       {selected === null && <option value="">Select a location</option>}
       {locations.map((loc) => (
-          <option key={loc.name} value={loc.name}>
-            {loc.name}
-          </option>
-        ))
-      }
+        <option key={loc.name} value={loc.name}>
+          {loc.name}
+        </option>
+      ))}
     </select>
   </div>
 );
