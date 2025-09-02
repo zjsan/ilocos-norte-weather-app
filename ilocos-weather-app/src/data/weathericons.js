f37d76cfd774ae4e7321fd5debb468cb0e187359
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-09-02 15:53:22 +0800

    Update weathericons.js

--- ilocos-weather-app/src/data/weathericons.js
+++ ilocos-weather-app/src/data/weathericons.js
@@ -1,5 +1,5 @@
 // weatherIcons.js
-import { Sun, Cloud, CloudRain, CloudSnow, CloudLightning } from "lucide-react";
+import { Sun, Cloud, CloudRain, CloudLightning } from "lucide-react";
 
 export const weatherIcons = {
   0: <Sun className="w-8 h-8 text-yellow-400 mx-auto" />, // Clear sky
@@ -10,7 +10,5 @@ export const weatherIcons = {
   48: <Cloud className="w-8 h-8 text-gray-400 mx-auto" />, // Fog with rime
   51: <CloudRain className="w-8 h-8 text-blue-400 mx-auto" />, // Drizzle
   61: <CloudRain className="w-8 h-8 text-blue-500 mx-auto" />, // Rain
-  71: <CloudSnow className="w-8 h-8 text-blue-200 mx-auto" />, // Snow
   95: <CloudLightning className="w-8 h-8 text-yellow-300 mx-auto" />, // Thunderstorm
-  // ... you can expand for all codes from Open-Meteo
 };
